f8e550a576fcf2224841148440eb83b8162b1da7
AlexHomePC <gotlib14@gmail.com>
2022-06-25 12:03:49 +0300

    .

--- HangmanGame/GameStart.cs
+++ HangmanGame/GameStart.cs
@@ -13,34 +13,33 @@ namespace HangmanGame
         MediumWord mediumWord;
         HardWord hardWord;
         StringBuilder result;
-        GameMananger gameMananger;
         private char[] _playerWord;
 
         public GameStart(LevelChoses ls)
         {
             result = new StringBuilder();
-            gameMananger = new GameMananger(); //Added to display a lvl Chose
-            easyWord = new EasyWord(random.Next(3, 5));
+
+            easyWord = new EasyWord(random.Next(3, 5)); // When game was created, those fields contein some random word.
             mediumWord = new MediumWord(random.Next(5, 7));
             hardWord = new HardWord(random.Next(7, 9));
 
             if(ls == LevelChoses.easyLvl)
             {
                 result = resultStartSet(easyWord);
-                _playerWord = easyWord.Word;
-                 
+                _playerWord = easyWord.Word;  
             }
             else if(ls == LevelChoses.mediumLvl)
             {
                 result = resultStartSet(mediumWord);
                 _playerWord = mediumWord.Word;
-            }else if(ls == LevelChoses.hardLvl)
+            }
+            else if(ls == LevelChoses.hardLvl)
             {
                 result = resultStartSet(hardWord);
                 _playerWord = hardWord.Word;
             }
         }
-        public int letterCheck(char c, char[] word)  //ref numm for counter double chares
+        public int letterCheck(char c, char[] word)  
         {
             int count = 0;
             for(int i = 0; i < word.Length; i++)
@@ -51,14 +50,13 @@ namespace HangmanGame
                     count++;
                 }
             }
-            
-           return count;
+           return count;  
         }
         public StringBuilder resultStartSet(WordClass word)
         {
              StringBuilder sb = new StringBuilder();
 
-            for(int i = 0; i < word.Word.Length*2; i++)
+            for(int i = 0; i < word.Word.Length*2; i++)  // Without this field, result field looks like this "___" ,and I wanted to make some space between letters "_ _ _"
             {
                 if (i % 2 == 0)
                 {
